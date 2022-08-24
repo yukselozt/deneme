cf0e3b724778f2e108348954c37fbb7e081f9427
Yüksel ÖZTÜRK <ykslztrk9@gmail.com>
2022-08-24 08:32:50 +0300

    ui

---
+++ UI/ui/src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <table id='table' border="1">
+    <tr>
+      <th></th>
+      <th>Food Name</th>
+      <th>Days Since I Ate</th>
+      <th>Kind</th> 
+      <th>Edit</th>   
+    </tr>
+    <tr>
+      <td></td>
+      <td>Apple</td>
+      <td>1</td>
+      <td>Sugar</td>
+      <td><button>Düzenle</button></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td>Potato</td>
+      <td>3</td>
+      <td>Carbonhydride</td>
+      <td><button>Düzenle</button></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td>Banana</td>
+      <td>2</td>
+      <td>Sugar</td>
+      <td><button>Düzenle</button></td>
+
+    </tr>
+    <tr>
+      <td></td>
+      <td>Cake</td>
+      <td>5</td>
+      <td>Carbonhydride</td>
+      <td><button>Düzenle</button></td>
+
+    </tr>
+    <tr>
+      <td></td>
+      <td>Pasta</td>
+      <td>7</td>
+      <td>Carbonhydride</td>
+      <td><button>Düzenle</button></td>
+
+    </tr>
+    </table>
+    </>
+  );
+}
+
+export default App;
